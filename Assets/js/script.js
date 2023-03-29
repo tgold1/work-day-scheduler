b7630778ebbbae36a73ef42e4cc5a08cb567e77b
Tim Gold <timothygold@comcast.net>
2023-03-29 17:10:15 -0400

    added date and time to script.js file and index.html

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,7 +1,16 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+var timer = document.getElementById("timeTracker");
 $(function () {
+
+function displayTime () {
+  var now = dayjs().format('MMMM, D, YYYY hh:mm:ss a');
+  timer.textContent = now;
+}
+displayTime ();
+setInterval(displayTime, 1000);
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
